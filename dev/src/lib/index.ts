3764dab2a8a1f58e9aa21f05b77a84cf88629fa8
Andrey Pavlenko <31999255+andrey-pavlenko@users.noreply.github.com>
2022-03-25 14:28:15 +0300

    Update 03-25-2022 14:28:15

--- dev/src/lib/index.ts
+++
@@ -1,3 +0,0 @@
-export { default as ColorPicker } from './components/ColorPicker.svelte';
-export { default as ColorInput } from './components/ColorInput.svelte';
-export * as converter from './converter';
